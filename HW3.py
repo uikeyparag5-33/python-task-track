6d21487659474543422817dec91cfd6637391e7d
parag uikey <uikeyparag5@gmail.com>
2026-08-06 11:28:52 +0530

    Add files via upload

--- HW3.py
+++ HW3.py
@@ -4,3 +4,7 @@ v3=100
 # memorydectection
 print(v1 == v2)
 print(v1 == v3)
+# id function 
+print(id(v1))
+print(id(v2))
+print(id(v3))
---
+++ HW4.py
@@ -0,0 +1,28 @@
+# the following task on date 5 aug on the function input 
+
+
+
+print("---------------my id card --------------")
+
+
+name = input("enter your name = ")
+
+
+age = input("enter your age = ")
+
+
+city = input("enter your city = ")
+
+
+collage = input("enter your collage = ")
+
+
+blood_group = input("enter your blood group = ")
+
+
+
+print(id(name), " Name : ", name)
+print(id(age), "   age : ", age)
+print(id(city), "   city : ", city)
+print(id(collage), "   collage : ", collage)
+print(id(blood_group), "   blood group : ", blood_group)
